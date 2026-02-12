9d947843bd891f4e0ad8bbe77e9e3ae11e6911ac
Aromatic <symwww@outlook.com>
2026-02-12 11:37:41 +0800

    fix undefined symbol: selinux

--- local/builder_6.6.89_mtk.sh
+++ local/builder_6.6.89_mtk.sh
@@ -468,8 +468,8 @@ if [[ "$APPLY_BBG" == "y" || "$APPLY_BBG" == "Y" ]]; then
     }
   }
   ' ./Kconfig > Kconfig.tmp && mv Kconfig.tmp ./Kconfig
-  sed -i 's/selinuxfs.o //g' "./selinux/Makefile"
-  sed -i 's/hooks.o //g' "./selinux/Makefile"
+  # sed -i 's/selinuxfs.o //g' "./selinux/Makefile"
+  # sed -i 's/hooks.o //g' "./selinux/Makefile"
   cat "./baseband-guard/sepatch.txt" >> "./selinux/Makefile"
   cd ../../
 fi
